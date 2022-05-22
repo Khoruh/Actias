f01327868d7b196c3e8a9806ccba263187a0ff84
Khora <KhoraBusiness@hotmail.com>
2022-05-22 13:24:07 -0300

    stupid things i missed

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const { REST } = require('@discordjs/rest');
 const fs = require('node:fs');
 const { Routes } = require('discord-api-types/v10');
-const { clientId, guildId, token } = require('./configFiles/config.json');
+const { clientId, testServerId, RoseGardenId, token } = require('./configFiles/config.json');
 
 const commands = [];
 const commandsPath = './commands';
@@ -18,6 +18,6 @@ const rest = new REST({ version: '10' }).setToken(token);
 rest.put(Routes.applicationGuildCommands(clientId, testServerId), { body: commands })
 	.then(() => console.log('Successfully registered application to Testing.'))
 	.catch(console.error);
-	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	rest.put(Routes.applicationGuildCommands(clientId, RoseGardenId), { body: commands })
 	.then(() => console.log('Successfully registered application to The Rose Garden.'))
 	.catch(console.error);
