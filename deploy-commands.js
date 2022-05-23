c4aac881b5055318e53f7cb0baaaccc57c88ee50
Khora <KhoraBusiness@hotmail.com>
2022-05-23 13:29:58 -0300

    Replace clientId with client.user.id in command registry

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,9 @@ const { REST } = require('@discordjs/rest');
 const fs = require('node:fs');
 const { Routes } = require('discord-api-types/v10');
 const { clientId, testServerId, RoseGardenId, token } = require('./configFiles/config.json');
-
+const client = new Discord.Client({
+	intents: new Discord.Intents(32767)
+  });
 const commands = [];
 const commandsPath = './commands';
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -25,9 +27,9 @@ for (var file of contextFiles) {
 const rest = new REST({ version: '10' }).setToken(token);
 const pushCommands = commands.concat(contextMenus)
 rest.put(
-	Routes.applicationGuildCommands(clientId, testServerId), { body: pushCommands })
+	Routes.applicationGuildCommands(`${client.user.id}`, testServerId), { body: pushCommands })
 	.then(() => console.log('Successfully registered application to Testing.'))
 	.catch(console.error);
-	rest.put(Routes.applicationGuildCommands(clientId, RoseGardenId), { body: pushCommands })
+	rest.put(Routes.applicationGuildCommands(client.user.id, RoseGardenId), { body: pushCommands })
 	.then(() => console.log('Successfully registered application to The Rose Garden.'))
 	.catch(console.error);
